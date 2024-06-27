e096a9dabbbd5ec1bde086b4f64f457f550f6822
nagendragt12 <nagendragt499@gmail.com>
2024-06-27 21:42:50 +0530

    push the code into git

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:nagendragt499@email.com">nagendragt499@email.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="www.linkedin.com/in/nagendrachami">linkedin.com/nagendrachami</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/nagendragt12">github.com/nagendragt</a>
+        </li>
+        {/* <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/nagendragt12">github.com/nagendragt</a>
+        </li> */}
+      </ul>
+    </footer>
+  );
+};
